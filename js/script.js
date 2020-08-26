b6712b3a3fa5954fddf6fbf841e3300eed804e95
nya3_neko2 <takahashinaoki521@gmail.com>
2020-08-26 19:27:16 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('readed')
+var search_link = "https://twitter.com/search?q="
+// var keyword = document.getElementById("input_keyword").value;
+
+function SearchButtonClick() {
+    console.log(search_link);
+}
